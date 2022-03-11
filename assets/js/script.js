d732e8b14f0651f307eb3cc1c7395793c8d02514
yeaty kponou <yeatykponou@yeatys-mbp.myfiosgateway.com>
2022-03-11 12:13:22 -0500

    updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+buttonEl.addEventListener("click", function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
+
+var buttonEl = document.querySelector("#save-task");
+console.log (buttonEl);
